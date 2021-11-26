f1f4303aa4ffccc00018c9a128ff699b360dabc5
Jefferson Follmann <jeff.follmann@gmail.com>
2021-11-26 16:15:04 -0300

    Add easter-eggs

--- scripts/main.js
+++ scripts/main.js
@@ -29,50 +29,44 @@ document.addEventListener('DOMContentLoaded', () => {
   toggleSwitch.onclick = toggleSwitchClick;
 });
 
-const eggs = () => {
-  "use strict"
-
+const loadEggs = () => {
   console.log('🚀 Try typing konami code sequence: ↑ ↑ ↓ ↓ ← → ← → 🅱 🅰');
-  let key = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
-  let ck = 0
-  let max = key.length
+  let key = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
+  let ck = 0;
+  let max = key.length;
 
   const showEggs = () => {
-    var img = new Image()
-    img.src = 'https://weichiachang.github.io/easter-eggs-mobile/images/mario.gif'
+    var img = new Image();
+    img.src = 'https://weichiachang.github.io/easter-eggs-mobile/images/mario.gif';
     img.style.pointerEvents = "none";
-    img.style.width = '250px'
-    img.style.height = '200px'
-    img.style.transition = '6s all linear'
-    img.style.position = 'fixed'
-    img.style.left = '-400px'
-    img.style.bottom = '0'
-    img.style.zIndex = 999999
+    img.style.width = '250px';
+    img.style.height = '200px';
+    img.style.transition = '6s all linear';
+    img.style.position = 'fixed';
+    img.style.left = '-400px';
+    img.style.bottom = '0';
+    img.style.zIndex = 999999;
 
-    document.body.appendChild(img)
-    window.setTimeout(function () {
-      img.style.left = 'calc(100% + 500px)'
-    }, 50)
+    document.body.appendChild(img);
+    window.setTimeout(() => {
+      img.style.left = 'calc(100% + 500px)';
+    }, 50);
 
-    window.setTimeout(function () {
-      img.parentNode.removeChild(img)
-    }, 6000)
+    window.setTimeout(() => {
+      img.parentNode.removeChild(img);
+    }, 6000);
   }
 
   const record = (e) => {
-    if (e.which === key[ck]) {
-      ck++
-    } else {
-      ck = 0
-    }
+    e.which === key[ck] ? ck++ : ck = 0;
 
     if (ck >= max) {
-      showEggs()
-      ck = 0
+      showEggs();
+      ck = 0;
     }
   }
 
   document.addEventListener('keyup', record);
 }
 
-eggs();
+loadEggs();
